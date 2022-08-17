0f7d548fdc5df63cbaeeab723a2a06ae9a6481c2
manne65-hd <manne@hoffmaenner.net>
2022-08-17 16:17:55 +0200

    Adding german translation

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'NEW automatic action: Assign automatically a priority based on a category' => 'NEUE automatische Aktion: Automatisch eine Priorität anhand der Kategorie zuweisen',
+    'Assign automatically a priority based on a category' => 'Automatisch eine Priorität anhand der Kategorie zuweisen'
+);
