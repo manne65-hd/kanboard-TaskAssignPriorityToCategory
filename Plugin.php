94557608ac9d3456badf6450f6cc94dad3943d88
manne65-hd <manne@hoffmaenner.net>
2022-08-17 15:54:22 +0200

    Adding Plugin-declaration

---
+++ Plugin.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kanboard\Plugin\TaskAssignPriorityToCategory;
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Plugin\TaskAssignPriorityToCategory\Action\TaskAssignPriorityToCategory;
+
+use Kanboard\Core\Translator;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+      $this->actionManager->register(new TaskAssignPriorityToCategory($this->container));
+    }
+
+    public function onStartup() {
+        // load Translation
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__ . '/Locale');
+    }
+
+    public function getPluginName()
+    {
+        return 'TaskAssignPriorityToCategory';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Adds an automatic action, that allows you to assign a priority based on a category');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Manfred Hoffmann';
+    }
+
+    public function getPluginVersion()
+    {
+        return '0.8.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/manne65-hd/kanboard-TaskAssignPriorityToCategory';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>=1.2.19';
+    }
+}
